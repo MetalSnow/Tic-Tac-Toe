8e7fb0c1354af56cc713a8982eeac7bcb59523c6
MetalSnow <yasuotrend@gmail.com>
2024-03-01 20:35:40 +0800

    Add winning condition logic

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const GameBoard = (function () {
-  let board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  let board = ["O", "O", "O", "", "", "", "", "", ""];
 
   const getBoard = () => board;
 
@@ -16,10 +16,10 @@ const Player = function (name, choice, position) {
 
 const GameController = function () {
   let gameBoard = GameBoard.getBoard();
-  const playerOne = Player("aymane", 1, 0);
-  const playerTwo = Player("saigou", 2, 2);
+  const playerOne = Player("aymane", "X", 0);
+  const playerTwo = Player("saigou", "O", 2);
 
-  let activePlayer = playerTwo;
+  let activePlayer = playerOne;
 
   const switchTurn = () => {
     activePlayer = activePlayer === playerOne ? playerTwo : playerOne;
@@ -38,7 +38,37 @@ const GameController = function () {
     }
   };
 
-  const gameWinner = function () {};
+  //winning condition
+  let winningCondition = [
+    [0, 1, 2], // Top row
+    [3, 4, 5], // Middle row
+    [6, 7, 8], // Bottom row
+    [0, 3, 6], // Left column
+    [1, 4, 7], // Middle column
+    [2, 5, 8], // Right column
+    [0, 4, 8], // Diagonal
+    [2, 4, 6], // Diagonal
+  ];
 
-  return { switchTurn, getActivePlayer, addSignToCell };
+  const checkWinner = function () {
+    for (comb of winningCondition) {
+      if (
+        gameBoard[comb[0]] === "X" &&
+        gameBoard[comb[1]] === "X" &&
+        gameBoard[comb[2]] === "X"
+      ) {
+        console.log(`${activePlayer.getName()} has won!`);
+      } else if (
+        gameBoard[comb[0]] === "O" &&
+        gameBoard[comb[1]] === "O" &&
+        gameBoard[comb[2]] === "O"
+      ) {
+        console.log(`${activePlayer.getName()} has won!`);
+      }
+    }
+  };
+
+  return { switchTurn, getActivePlayer, addSignToCell, checkWinner };
 };
+
+GameController();
