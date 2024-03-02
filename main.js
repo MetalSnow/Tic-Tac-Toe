0deec552cc9ab15ef7710e9fa80955bbd5d2842b
MetalSnow <yasuotrend@gmail.com>
2024-03-02 19:40:00 +0800

    -Add rendering functionality to DisplayController for the Tic Tac Toe game board -Implement game initialization and start functionality in GameController

--- main.js
+++ main.js
@@ -1,23 +1,37 @@
 const GameBoard = (function () {
-  let board = ["O", "O", "O", "", "", "", "", "", ""];
+  let board = ["", "", "", "", "", "", "", "", ""];
 
   const getBoard = () => board;
 
   return { getBoard };
 })();
 
-const Player = function (name, choice, position) {
+const Player = function (name, choice) {
   const getName = () => name;
   const getChoice = () => choice;
-  let isPosition = position;
 
-  return { getName, getChoice, isPosition };
+  return { getName, getChoice };
 };
 
 const GameController = function () {
-  let gameBoard = GameBoard.getBoard();
-  const playerOne = Player("aymane", "X", 0);
-  const playerTwo = Player("saigou", "O", 2);
+  const gameBoard = GameBoard.getBoard();
+
+  const startDiv = document.querySelector(".start-game");
+  const inputOne = document.querySelector("#player-one").value;
+  const inputTwo = document.querySelector("#player-two").value;
+  const turn = document.querySelector(".turn");
+  const stats = document.querySelector(".stats");
+
+  //initialize Game
+  function startGame() {
+    //Create Players
+    const playerOne = Player(inputOne, "X");
+    const playerTwo = Player(inputTwo, "O");
+
+    turn.textContent = `--${playerOne.getName()}'s Turn--`;
+    stats.textContent = `${playerOne.getName()}(${playerOne.getChoice()}) VS ${playerTwo.getName()}(${playerTwo.getChoice()})`;
+    startDiv.classList.add("active");
+  }
 
   let activePlayer = playerOne;
 
@@ -27,13 +41,12 @@ const GameController = function () {
 
   const getActivePlayer = () => activePlayer;
 
-  const addSignToCell = function () {
-    if (gameBoard.getBoard()[activePlayer.isPosition] !== "") {
+  const addSignToCell = function (cell) {
+    if (gameBoard[cell] !== "") {
       return;
     } else {
-      gameBoard
-        .getBoard()
-        .splice(activePlayer.isPosition, 1, activePlayer.getChoice());
+      gameBoard.splice(cell, 1, activePlayer.getChoice());
+      turn.textContent = `--${playerTwo.getName()}'s Turn--`;
       switchTurn();
     }
   };
@@ -51,24 +64,58 @@ const GameController = function () {
   ];
 
   const checkWinner = function () {
+    console.log(gameBoard);
     for (comb of winningCondition) {
       if (
         gameBoard[comb[0]] === "X" &&
         gameBoard[comb[1]] === "X" &&
         gameBoard[comb[2]] === "X"
       ) {
-        console.log(`${activePlayer.getName()} has won!`);
+        console.log(`${activePlayer.getName()} 1 has won!`);
       } else if (
         gameBoard[comb[0]] === "O" &&
         gameBoard[comb[1]] === "O" &&
         gameBoard[comb[2]] === "O"
       ) {
-        console.log(`${activePlayer.getName()} has won!`);
+        console.log(`${activePlayer.getName()} 2 has won!`);
+      } else if (!gameBoard.includes("")) {
+        return console.log("It's A Tie");
       }
     }
   };
 
-  return { switchTurn, getActivePlayer, addSignToCell, checkWinner };
+  return { getActivePlayer, addSignToCell, checkWinner, startGame };
 };
 
-GameController();
+const DisplayController = function () {
+  const gameBoard = GameBoard.getBoard();
+  const game = GameController();
+
+  const renderBoard = () => {
+    //Get DOM Elements
+    const startBtn = document.querySelector("#start");
+    const boardDiv = document.querySelector(".game-board");
+
+    //Initialize game
+    startBtn.addEventListener("click", game.startGame);
+
+    //Create Board
+    for (let i = 0; i < gameBoard.length; i++) {
+      let cell = document.createElement("button");
+
+      cell.classList.add("cell");
+      cell.dataset.index = i;
+
+      cell.addEventListener("click", () => {
+        game.addSignToCell(i);
+        cell.textContent = gameBoard[i];
+        console.log(gameBoard);
+      });
+      boardDiv.appendChild(cell);
+    }
+  };
+
+  renderBoard();
+};
+
+DisplayController();
